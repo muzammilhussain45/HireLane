9d0d7ac3d02d36e3d7d3c14d38656b050d09ec85
Muzammil Hussain <muzammilch2021@gmail.com>
2026-08-07 21:49:09 -0700

    Connection with db

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
+        });
+
+        await mongoose.connect(process.env.MONGO_URI);
+    } catch (error) {
+        console.error('MongoDB Connection Error:', error);
+    }
+};
+
+export default connectDB;
