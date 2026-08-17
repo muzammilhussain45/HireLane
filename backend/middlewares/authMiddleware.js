a75027bedf1f1597dc0ca76caf9290ec6ec269f5
Muzammil Hussain <muzammilch2021@gmail.com>
2026-08-17 17:04:18 +0500

    Adding auth middleware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+
+
+export const authMiddleware = (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1]; // Get token from Authorization header
+        if (!token) {
+            return res.status(401).json({ success: false, message: "No token provided" });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }
+    catch (error) {
+        return res.status(401).json({ success: false, message: "Invalid token or expired token" });
+
+    }
+}
+
+// to authorize based on user role
+
+export const authorize = (...roles) => {
+    return (req, res, next) => {
+        if (!req.user || !roles.includes(req.user.role)) {
+            return res.status(403).json({ success: false, message: "Access denied" });
+        }
+        next();
+    }
+}
