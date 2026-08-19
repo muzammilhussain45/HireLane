8ff17894f26fd8d2160b48db7b2765a1e4f634b4
Muzammil Hussain <muzammilch2021@gmail.com>
2026-08-19 18:57:10 +0500

    Adding Add job page by admin

---
+++ admin/src/pages/AddJobs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import AddJobsPage from '../components/AddJobsPage.jsx'
+
+const AddJobs = () => {
+  return (
+    <div>
+      <Navbar/>
+      <AddJobsPage />
+    </div>
+  )
+}
+
+export default AddJobs
