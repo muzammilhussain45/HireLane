9942717b7db5cc0ab53f3e3df1dbe0709429c41f
Muzammil Hussain <muzammilch2021@gmail.com>
2026-08-20 07:54:53 +0500

    UI for showing added company question is implemented

---
+++ admin/src/pages/ListCompanyQs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import ListCompanyQuestion from '../components/ListCompanyQuestion'
+
+
+const ListCompanyQs = () => {
+  return (
+    <div>
+      <Navbar/>
+      <ListCompanyQuestion/>
+    </div>
+  )
+}
+
+export default ListCompanyQs
