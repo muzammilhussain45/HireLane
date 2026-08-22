852b66427b57fc7991a9c3e75b17cca0c854b67f
Muzammil Hussain <muzammilch2021@gmail.com>
2026-08-22 11:10:57 +0500

    Adding UI to show user's saved jobs

---
+++ frontend/src/pages/Saved.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import SavePage from "../components/SavePage";
+
+
+const Saved = () => {
+  return (
+    <div>
+      <Navbar/>
+      <SavePage/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default Saved;
