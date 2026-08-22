a806aaf7cbc971cfc21c2a28339f54e4c54069f5
Muzammil Hussain <muzammilch2021@gmail.com>
2026-08-22 05:44:04 +0500

    Company page is added

---
+++ frontend/src/pages/Company.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import CompanyPage from "../components/CompanyPage";
+
+
+const Company = () => {
+  return (
+    <div>
+      <Navbar />
+      <CompanyPage />
+      <Footer />
+    </div>
+  );
+};
+
+export default Company;
