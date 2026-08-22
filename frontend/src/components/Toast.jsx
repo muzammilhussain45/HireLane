0b1b2aedc6dae18b93f088fff0bfd279dd866970
Muzammil Hussain <muzammilch2021@gmail.com>
2026-08-22 11:08:50 +0500

    Adding toast component

---
+++ frontend/src/components/Toast.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import {toastStyles as s} from '../assets/dummyStyles'; 
+import {
+  CheckCircle2,
+  XCircle,
+  X
+} from 'lucide-react';
+
+const Toast = ({ type , message, onClose, duration = 3000 }) => {
+  // Auto-dismiss timer logic
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+        onClose();
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, [duration, onClose]);
+
+  // Toast variant configurations
+  const config = {
+    success: {
+      icon: <CheckCircle2 className="w-5 h-5 text-emerald-500" />,
+      ...s.variants.success
+    },
+    error: {
+      icon: <XCircle className="w-5 h-5 text-rose-500" />,
+      ...s.variants.error
+    }
+  };
+
+  const { icon, bg, border, text } = config[type] || config.success;
+
+  return (
+    <div className={`${s.container} ${bg} ${border} ${text}`}>
+      <div className={s.iconWrapper}>
+        {icon}
+      </div>
+
+      <p className={s.message}>{message}</p>
+
+      <button onClick={onClose} className={s.closeButton}>
+        <X className={s.closeIcon} />
+      </button>
+
+      <style>{s.animations}</style>
+    </div>
+  );
+};
+
+export default Toast;
