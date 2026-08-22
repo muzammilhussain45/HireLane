883e86acbba18cbdedba8612a6853abc36168cb4
Muzammil Hussain <muzammilch2021@gmail.com>
2026-08-22 06:00:08 +0500

    Role page is added

---
+++ frontend/src/pages/Roles.jsx
@@ -0,0 +1,17 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import RolePage from "../components/RolePage";
+
+
+const Roles = () => {
+  return (
+    <div>
+      <Navbar/>
+      <RolePage/>   
+      <Footer/>
+
+    </div>
+  );
+};
+
+export default Roles;
